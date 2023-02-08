ffd1539e862d65c1312c40861fe7097221d2e3f7
Ashley Mojica <ashjmojica@gmail.com>
2023-02-08 11:22:35 -0600

    done

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const [isInStock, setIsInStock] = useState(true);
+  const {name, image, price} = plant;
+
+function handleToggleStock() {
+  setIsInStock((isInStock) => !isInStock);
+}
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
+      <img src={image} alt={name} />
       <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <p>Price: {price}</p>
       {true ? (
-        <button className="primary">In Stock</button>
+        <button className="primary" onClick={handleToggleStock}>In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
     </li>
   );
 }
+
+
+
+
 
 export default PlantCard;
